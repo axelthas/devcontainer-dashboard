4f3fc56541a53f1a45bcb392d2451cfbbe0c2c6e
Axel Thor Asthorsson <axel@tern.is>
2026-07-09 09:20:53 +0000

    Update script permissions and improve variable quoting in run-dashboard script

--- scripts/run-dashboard.sh
+++ scripts/run-dashboard.sh
@@ -5,7 +5,7 @@ CONTAINER_SSH_SOCKET=/tmp/ssh-agent.socket
 DOCKER_SOCKET=/var/run/docker.sock
 
 DEFAULT_WORKSPACE="$HOME/ddash-workspace"
-read -r -p "Workspace directory [${DEFAULT_WORKSPACE}]: " WORKSPACE_ROOT
+read -r -p "Workspace directory [${DEFAULT_WORKSPACE}]: " WORKSPACE_ROOT </dev/tty
 WORKSPACE_ROOT="${WORKSPACE_ROOT:-$DEFAULT_WORKSPACE}"
 WORKSPACE_ROOT="${WORKSPACE_ROOT/#\~/$HOME}"
 
@@ -16,11 +16,11 @@ docker pull "$IMAGE"
 
 docker run  --rm \
             -e HOST_HOSTNAME=$HOSTNAME \
-            -e WORKSPACE_ROOT=$WORKSPACE_ROOT \
+            -e WORKSPACE_ROOT="$WORKSPACE_ROOT" \
             -e SSH_AUTH_SOCK=$CONTAINER_SSH_SOCKET \
-            -v $WORKSPACE_ROOT:$WORKSPACE_ROOT \
+            -v "$WORKSPACE_ROOT:$WORKSPACE_ROOT" \
             -v $SSH_AUTH_SOCK:$CONTAINER_SSH_SOCKET \
             -v $DOCKER_SOCKET:$DOCKER_SOCKET \
             -p 3000:3000 \
             --name devcontainer-dashboard \
-            $IMAGE
+            "$IMAGE"
